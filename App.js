3acf7dd0e76b7cec0c8ad206771a8206c8c1bab7
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-12 13:32:39 -0500

    Pass the selected number from StartGameScreen to App

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from './components/Header'
@@ -6,6 +6,11 @@ import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from './screens/GameScreen'
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber)
+  }
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
