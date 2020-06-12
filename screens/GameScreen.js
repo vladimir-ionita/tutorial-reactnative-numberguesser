12f0bbc70a23053bd8e04e4343ab8012267f2ca1
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-12 13:57:27 -0500

    Pass the number of rounds from the GameScreen to the App

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { View, Text, StyleSheet, Button, Alert } from 'react-native'
 
 import NumberContainer from '../components/NumberContainer'
@@ -19,8 +19,17 @@ const generateRandomBetween = (min, max, exclude) => {
 
 const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+    const [rounds, setRounds] = useState(0)
     const currentLow = useRef(1)
     const currentHigh = useRef(100)
+
+    const { userChoice, onGameOver } = props
+
+    useEffect(() => {
+        if (currentGuess === userChoice) {
+            onGameOver(rounds)
+        }
+    }, [currentGuess, userChoice, onGameOver])
 
     const nextGuessHandler = direction => {
         if (
@@ -41,6 +50,7 @@ const GameScreen = props => {
 
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
         setCurrentGuess(nextNumber)
+        setRounds(currentRounds => currentRounds + 1)
     }
 
     return (
