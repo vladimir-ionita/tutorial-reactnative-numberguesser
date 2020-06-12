fbcaeb7cafe24e098f0281eb9e07c30f1f936dcc
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-12 13:31:22 -0500

    Create `GameScreen`

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 
 import Header from './components/Header'
 import StartGameScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
 
 export default function App() {
   return (
