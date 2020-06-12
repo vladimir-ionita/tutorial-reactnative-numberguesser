10d7a018f7cdd5726c1cf93e6a38355ff9b1060f
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-12 12:10:59 -0500

    Add a shadow to the input container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,7 +30,15 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',                   // iOS-specific
+        shadowOffset: { width: 0, height: 2},   // iOS-specific
+        shadowRadius: 6,                        // iOS-specific
+        shadowOpacity: 0.26,                    // iOS-specific
+        elevation: 5,                           // android-specific
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 20
     },
     buttonsContainer: {
         flexDirection: 'row',
